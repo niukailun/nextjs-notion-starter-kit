0b9469d3536855f3ff0be1d8da45c8eb99fd1fdc
19567394 <19567394@student.westernsydney.edu.au>
2024-02-13 16:23:52 +1100

    change

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://observant-roast-ac2.notion.site/Quick-Note-f05c663bac924445a909b416367e21fd?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
